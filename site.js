dadd7c8337cc311be6c8220f63ff380f71461716
WolfDab <BuildingWolf100@outlook.com>
2024-01-30 20:46:15 -0600

    Added welcome div

---
+++ site.js
@@ -0,0 +1,15 @@
+const hours = new Date().getHours(); // get the current hour
+
+const isMorning = hours >= 4 && hours < 12; // is it morning?
+const isAfternoon = hours >= 12 && hours < 17; // is it afternoon?
+const isEvening = hours >= 17 || hours < 4; // is it evening?
+
+const time = document.querySelector("#time");
+
+if (isMorning) {
+    time.textContent = "Good morning!";
+} else if (isAfternoon) {
+    time.textContent = "Good afternoon!";
+} else {
+    time.textContent = "Good evening!";
+}
